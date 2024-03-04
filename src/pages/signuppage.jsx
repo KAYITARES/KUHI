30e07a54b5377630f55359110be259f828863a02
Franklin-pro <Franklin-pro>
2024-03-04 12:57:35 +0200

    add

---
+++ src/pages/signuppage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUp from "../components/signup";
+
+
+
+function SignUpPage(){
+    return (
+        <>
+            <SignUp/>
+        </>
+    )
+}
+
+export default SignUpPage
