8efdb441ee254eb541277ce5e3ff67ec65df8045
Franklin-pro <Franklin-pro>
2024-03-05 19:54:03 +0200

    changes

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -4,7 +4,20 @@ import React from "react";
 function MembersMedia(){
     return (
         <>
-       
+       <div className="media-container">
+<div className="madia-delay">
+    <div className="medias">
+        <div className="organize">
+<h3>alight organisation</h3>
+<h3>kuhi</h3>
+<h3>world vision</h3>
+        </div>
+        <div className="photo-grid">
+            
+            </div>
+    </div>
+</div>
+       </div>
         </>
     )
 }
