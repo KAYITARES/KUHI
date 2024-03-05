dee38660b11cb9f4bf594e30234e1dfd9cc72f5e
Gedeon-programmer <twizerimanagedeon273@gmail.com>
2024-03-05 18:22:41 +0200

    state

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -5,6 +5,7 @@ import HomePage from "../components/homepage";
 import Speakers from "../components/speakers";
 import Sponsors from "../components/sponsor";
 import Footer from "../components/footer";
+import Agend from "../components/agenda"
 
 
 function Welcome(){
@@ -13,6 +14,12 @@ function Welcome(){
             <NavBar/>
             <HomePage/>
             <Speakers/>
+            <div className="agenda">
+                <div className="heard">
+                    <h1>Our Agenda</h1>
+                </div>
+                <Agend/>
+            </div>
             <Sponsors/>
             <Footer/>
         </>
