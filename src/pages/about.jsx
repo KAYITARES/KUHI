33602646aee4ad1d943e5cd569d1bf3c066213cf
kagabojaphet <iradukundajaphet59@gmail.com>
2024-03-06 15:13:45 +0200

    add

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import About from "../components/about";
+import NavBar from "../components/navbar";
+import Footer from "../components/footer";
 
 const AboutUs=()=>{
     return(
         <>
+        <NavBar/>
         <About/>
+        <Footer/>
         </>
     )
     
